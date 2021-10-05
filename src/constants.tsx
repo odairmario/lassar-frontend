6546e908d25d4052a6a4fdb61af882ce1c8eb47f
Odair M <32972486+odairmario@users.noreply.github.com>
2021-10-05 08:31:14 -0300

    build: setup base url to env variable PUBLIC_URL (#3)
    doc: add deploy documentation
    
    doc: add nginx template

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,2 +1,2 @@
-export const API_URL="http://jas.c3sl.ufpr.br:8000"
-export const WEBSOCKET_ADDRESS="ws://jas.c3sl.ufpr.br:8000/record/"
+export const API_URL="http://localhost:8000"
+export const WEBSOCKET_ADDRESS="ws://localhost:8000/record/"
